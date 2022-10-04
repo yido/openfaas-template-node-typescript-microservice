cbe9e53ba01c80a85e8404e2960cceddbfff05dc
Yididiya Gebredingel <yidogeb@gmail.com>
2022-10-04 16:54:23 +0200

    Update index.ts

--- template/node-typescript-microservice/index.ts
+++ template/node-typescript-microservice/index.ts
@@ -27,6 +27,10 @@ app.use(function addDefaultContentType(req, res, next) {
 if (use_basic_auth)
     app.use(addBasicAuth);
 
+console.log("process.env.BASIC_AUTH",process.env.BASIC_AUTH);
+console.log("process.env.API_KEY_NAME",process.env.API_KEY_NAME);
+console.log("use_basic_auth",use_basic_auth);
+
 async function addBasicAuth(req, res, next) {
   if ('OPTIONS' == req.method && process.env.ENABLE_CORS) {
      next();
@@ -46,6 +50,9 @@ async function addBasicAuth(req, res, next) {
     res.send(msg);
     res.end();
   }
+  
+  console.log("apiKey --> ",apiKey);
+  console.log("auth --> ",auth);
   
   if (auth && auth == apiKey) {
     next();
